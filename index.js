f1519cc9947118ecd0e2cf390b839258d8cdff5a
Aisosa <allysonaghedo@gmail.com>
2023-09-09 06:31:04 +0100

    completed the hng stage 1 task for frontend track

---
+++ index.js
@@ -0,0 +1,32 @@
+function updateDate() {
+  const day = document.getElementById("day");
+  const utcTime = document.getElementById("time");
+
+  // Function to get and display the current day of the week and UTC time in milliseconds
+  function displayDate() {
+    const now = new Date();
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const currentDay = daysOfWeek[now.getDay()];
+    const utcMilli = now.getTime();
+
+    day.textContent = currentDay;
+    utcTime.textContent = utcMilli;
+  }
+
+  
+  displayDate();
+
+  
+  setInterval(displayDate, 1000);
+}
+
+
+updateDate();
